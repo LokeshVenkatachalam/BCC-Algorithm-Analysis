b08e2ad43975c00d55bdddf999fb6ef46f9909fe
LokeshVekatachalam <lokesh.v@research.iiit.ac.in>
2022-08-21 00:01:08 +0530

    Final Project Submission

---
+++ Check-Jen.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> graph;
+vector<bool> dfs_visited;
+vector<int> schmidt_visited;
+vector<int> dfs_discovery_time_vs_node;
+vector<int> dfs_discovery_node_vs_time;
+vector<vector<int>> dfs_tree;
+vector<vector<int>> back_edges;
+
+void setup(int n)
+{
+    graph.resize(n + 1);
+    dfs_tree.resize(n + 1);
+    back_edges.resize(n + 1);
+    dfs_visited.resize(n + 1);
+    schmidt_visited.resize(n + 1);
+    dfs_discovery_node_vs_time.resize(n + 1);
+    for (int i = 0; i < n + 1; i++)
+    {
+        dfs_visited[i] = false;
+        schmidt_visited[i] = false;
+    }
+}
+
+void input_graph(int e)
+{
+    for (int i = 0; i < e; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+}
+
+void print_graph()
+{
+    cout << "graph is \n";
+    for (int i = 1; i <= graph.size(); i++)
+    {
+        cout << i << ": ";
+        for (int j = 0; j < graph[i].size(); j++)
+        {
+            cout << graph[i][j] << ",";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void print_dfs_tree()
+{
+    cout << "dfs tree:\n";
+    for (int i = 1; i <= dfs_tree.size(); i++)
+    {
+        cout << i << ": ";
+        for (int j = 0; j < dfs_tree[i].size(); j++)
+        {
+            cout << dfs_tree[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+void print_back_edges()
+{
+    cout << "back edges:\n";
+    for (int i = 1; i <= back_edges.size(); i++)
+    {
+        cout << i << ": ";
+        for (int j = 0; j < back_edges[i].size(); j++)
+        {
+            cout << back_edges[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void dfs(int start, int parent)
+{
+    dfs_visited[start] = true;
+    dfs_discovery_time_vs_node.push_back(start);
+    dfs_discovery_node_vs_time[start] = dfs_discovery_time_vs_node.size() - 1;
+    for (int neighbour : graph[start])
+    {
+        if (dfs_visited[neighbour] == false)
+        {
+            dfs_tree[neighbour].push_back(start);
+            dfs(neighbour, start);
+        }
+        else
+        {
+            if (neighbour == parent)
+            {
+                continue;
+            }
+            if (dfs_discovery_node_vs_time[neighbour] < dfs_discovery_node_vs_time[start])
+            {
+                back_edges[neighbour].push_back(start);
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, e; // n = num of nodes, e = num of edges
+    cin >> n >> e;
+    setup(n);
+    input_graph(e);
+
+#ifdef PRINT_GRAPH
+    print_graph();
+#endif
+
+    dfs(1, -1);
+
+#ifdef PRINT_DFS_TREE
+    print_dfs_tree();
+#endif
+#ifdef PRINT_BACK_EDGES
+    print_back_edges();
+#endif
+    int num_cycles = 0;
+    for (auto ear_start_vertex : dfs_discovery_time_vs_node)
+    {
+        schmidt_visited[ear_start_vertex] = true;
+        for (auto backnode : back_edges[ear_start_vertex])
+        {
+            // cout << "ear: " << ear_start_vertex << ",";
+            // cout << backnode << ",";
+            auto next_node = backnode;
+            while (schmidt_visited[next_node] == false)
+            {
+                schmidt_visited[next_node] = true;
+                next_node = dfs_tree[next_node][0];
+                // cout << next_node << ",";
+                if (next_node == ear_start_vertex)
+                    num_cycles++;
+            }
+            // cout << "\n";
+        }
+    }
+    if (num_cycles == 1)
+        cout << "The only cycle was C1, so the graph is one entire biCC\n";
+    else if (num_cycles > 1)
+        cout << "There was another cycle other than C1, so the graph is has multiple biCC\n";
+}
---
+++ Tarzan-articulation-point-Biconnected-Components.cpp
@@ -0,0 +1,156 @@
+#include "bits/stdc++.h"
+using namespace std;
+int dfs_time = 0;
+int no_biconnected_components = 0;
+int no_of_vertex = 0;
+int no_of_edges = 0;
+vector<int> low, parent, discovery_time;
+vector<bool> visited;
+vector<vector<int>> adjacency_list;
+stack<pair<int, int>> stack1;
+set<int> set1;
+void DFS(int vertex)
+{
+  discovery_time[vertex] = dfs_time + 1;
+  low[vertex] = dfs_time + 1;
+  dfs_time = dfs_time + 1;
+  visited[vertex] = true;
+  int child = 0;
+  for (int i = 0; i < adjacency_list[vertex].size(); i++)
+  {
+    int child_vertex = adjacency_list[vertex][i];
+    if (visited[child_vertex] == false)
+    {
+      child = child + 1;
+      stack1.push(make_pair(vertex, child_vertex));
+      parent[child_vertex] = vertex;
+      DFS(child_vertex);
+      low[vertex] = min(low[vertex], low[child_vertex]);
+      if (parent[vertex] == -1 && child > 1)
+      {
+        no_biconnected_components = no_biconnected_components + 1;
+        cout << "biconnected_component_no: " << no_biconnected_components << '\n';
+        pair<int, int> temp_pair = make_pair(vertex, child_vertex);
+        while (stack1.top() != temp_pair)
+        {
+         // cout << stack1.top().first << " " << stack1.top().second << '\n';
+          set1.insert(stack1.top().first);
+          set1.insert(stack1.top().second);
+          stack1.pop();
+        }
+        //cout << stack1.top().first << " " << stack1.top().second << '\n';
+        set1.insert(stack1.top().first);
+        set1.insert(stack1.top().second);
+        stack1.pop();
+        for(auto itr : set1)
+        {
+          cout << itr << ' ';
+        }
+        cout << '\n';
+        set1.clear();
+      }
+      if (parent[vertex] != -1 && low[child_vertex] >= discovery_time[vertex])
+      {
+        no_biconnected_components = no_biconnected_components + 1;
+        cout << "biconnected_component_no: " << no_biconnected_components << '\n';
+        pair<int, int> temp_pair = make_pair(vertex, child_vertex);
+        while (stack1.top() != temp_pair)
+        {
+          // cout << stack1.top().first << " " << stack1.top().second << '\n';
+          set1.insert(stack1.top().first);
+          set1.insert(stack1.top().second);
+          stack1.pop();
+        }
+        // cout << stack1.top().first << " " << stack1.top().second << '\n';
+        set1.insert(stack1.top().first);
+        set1.insert(stack1.top().second);
+        stack1.pop();
+        for(auto itr : set1)
+        {
+          cout << itr << ' ';
+        }
+        cout << '\n';
+        set1.clear();
+      }
+    }
+    else if (parent[vertex] != child_vertex && discovery_time[child_vertex] < low[vertex])
+    {
+      low[vertex] = discovery_time[child_vertex];
+      stack1.push(make_pair(vertex, child_vertex));
+    }
+  }
+  return;
+}
+
+void biconnected_components()
+{
+  for (int i = 1; i <= no_of_vertex; i++)
+  {
+    if (visited[i] == false)
+    {
+      DFS(i);
+      if (!stack1.empty())
+      {
+        no_biconnected_components = no_biconnected_components + 1;
+        cout << "biconnected_component_no: " << no_biconnected_components << '\n';
+        while (!stack1.empty())
+        {
+         //cout << stack1.top().first << " " << stack1.top().second << '\n';
+         
+          set1.insert(stack1.top().first);
+          set1.insert(stack1.top().second);
+          stack1.pop();
+        }
+        for(auto itr : set1)
+        {
+          cout << itr << ' ';
+        }
+        cout << '\n';
+        set1.clear();
+      }
+    }
+  }
+  cout << "No of biconnected_components: " << no_biconnected_components << '\n';
+}
+void setup(int n)
+{
+  adjacency_list.resize(n+1);
+  // adjacency_list = new std::vector<int>[n + 1];
+  // *adjacency_list = malloc((sizeof(vector<int> *))*(n+1));
+  visited.resize(n + 1);
+  low.resize(n + 1);
+  parent.resize(n + 1);
+  discovery_time.resize(n + 1);
+  for (int i = 0; i < n + 1; i++)
+  {
+    visited[i] = false;
+    parent[i] = -1;
+    low[i] = INT_MAX - 1;
+  }
+}
+
+void input_graph(int e)
+{
+  // e = min(e,95000);
+  for (int i = 0; i < e; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    // cout<<i<<'\n';
+    adjacency_list[a].push_back(b);
+    adjacency_list[b].push_back(a);
+  }
+}
+int main()
+{
+  // freopen("output.txt", "r", stdin);
+  // freopen("test2.txt", "w", stdout);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin >> no_of_vertex;
+  cin >> no_of_edges;
+  setup(no_of_vertex);
+  input_graph(no_of_edges);
+  biconnected_components();
+  return 0;
+}
